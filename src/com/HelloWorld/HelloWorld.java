8769af106df99c88b4e0ead694f395d5bee02ad7
nikhilsharma29 <nik29.sharma@gmail.com>
2021-07-23 22:55:03 +0530

    chnages in master

--- src/com/HelloWorld/HelloWorld.java
+++ src/com/HelloWorld/HelloWorld.java
@@ -4,9 +4,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello India Testing in new branch A1");
-        System.out.println("Hello India Testing in new branch A2");
-        System.out.println("Hello India Testing in new branch A3");
-        System.out.println("Hello India Testing in new branch A4");
-        System.out.println("Hello India Testing in new branch A10");
+        System.out.println("Hello India Testing in new branch B2");
     }
 }
